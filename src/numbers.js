1c0865d7faa1ca9870a7fe0622d6965a5d4ac866
Cody Smith <37386486+oncody@users.noreply.github.com>
2023-01-19 23:37:54 -0700

    arrow functions and more unit tests

--- src/numbers.js
+++ src/numbers.js
@@ -1,44 +1,31 @@
 class Numbers {
-    /**
-     * Remove commas from number and then convert that number to a number object
+    /** Remove commas from number and then convert that number to a number object
      * @param {string} string
-     * @returns {number}
-     */
-    static fromString(string) {
-        let trimmedString = string.replace(/,/g, '');
-        return Number(trimmedString);
-    }
+     * @returns {number} */
+    static fromString = (string) => Number(string.replace(/,/g, ''));
 
-    /**
-     * Round a float to X decimal places
+    /** Round a float to X decimal places
      * @param {number} number
      * @param {number} decimals
-     * @returns {number}
-     */
+     * @returns {number} */
     static roundFloat = (number, decimals) => parseFloat(number.toFixed(decimals));
 
-    /**
-     *
-     * @param {number} min - inclusive
-     * @param {number} max - exclusive
-     * @returns {number}
-     */
-    static randomIntegerExclusive(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min) + min);
+    /** @param {number} minInclusive - inclusive
+     * @param {number} maxExclusive - exclusive
+     * @returns {number} */
+    static randomIntegerExclusive = (minInclusive, maxExclusive) => {
+        minInclusive = Math.ceil(minInclusive);
+        maxExclusive = Math.floor(maxExclusive);
+        return Math.floor(Math.random() * (maxExclusive - minInclusive) + minInclusive);
     }
 
-    /**
-     *
-     * @param {number} min - inclusive
-     * @param {number} max - inclusive
-     * @returns {number}
-     */
-    static randomIntegerInclusive(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1) + min);
+    /** @param {number} minInclusive - inclusive
+     * @param {number} maxInclusive - inclusive
+     * @returns {number} */
+    static randomIntegerInclusive = (minInclusive, maxInclusive) => {
+        minInclusive = Math.ceil(minInclusive);
+        maxInclusive = Math.floor(maxInclusive);
+        return Math.floor(Math.random() * (maxInclusive - minInclusive + 1) + minInclusive);
     }
 }
 
---
+++ test/numbers.test.js
@@ -0,0 +1,29 @@
+import {Numbers} from "../src/numbers.js";
+
+test('test numbers from string basic', () => {
+    expect(Numbers.fromString('3')).toBe(3);
+});
+
+test('test numbers from string with comma', () => {
+    expect(Numbers.fromString('1,234')).toBe(1234);
+});
+
+test('test numbers from string with two commas', () => {
+    expect(Numbers.fromString('1,234,567')).toBe(1234567);
+});
+
+test('test numbers round float', () => {
+    expect(Numbers.roundFloat(3.14159, 2)).toBe(3.14);
+});
+
+test('test numbers round float', () => {
+    expect(Numbers.roundFloat(3.14159, 3)).toBe(3.142);
+});
+
+test('test random int exclusive', () => {
+    expect(Numbers.randomIntegerExclusive(1, 2)).toBe(1);
+});
+
+test('test random int exclusive', () => {
+    expect(Numbers.randomIntegerInclusive(1, 1)).toBe(1);
+});
